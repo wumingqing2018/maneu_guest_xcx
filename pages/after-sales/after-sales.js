f4089f7a305b7fb6966130e598c7128f466bf8ce
wumq <759823845@qq.com>
2024-07-27 03:59:41 +0800

    update

--- pages/after-sales/after-sales.js
+++ pages/after-sales/after-sales.js
@@ -1,11 +1,33 @@
+// pages/orderDetail/orderDetail.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     "time": "",
-    "content": ""
+    "OD_ADD": "",
+    "OD_AX": "",
+    "OD_BCVA": "",
+    "OD_CYL": "",
+    "OD_FR": "",
+    "OD_PR": "",
+    "OD_SPH": "",
+    "OD_VA": "",
+    "OS_ADD": "",
+    "OS_AX": "",
+    "OS_BCVA": "",
+    "OS_CYL": "",
+    "OS_FR": "",
+    "OS_PR": "",
+    "OS_SPH": "",
+    "OS_VA": "",
+    "PD": "",
+    "store_function": "",
+    "remark": "",
+    "vision_remark": "",
+    "vision_content": [],
+    "store": [],
+    "time": ""
   },
 
   /**
@@ -16,66 +38,86 @@ Page({
       url: 'https://maneu.online/get_detail/',
       method: 'GET',
       data: {
-        'text': 'Servive',
+        'text': 'Order',
         'code': options.code
       },
       success: (res) => {
-        let content = res.data.content
+        console.log(res.data)
         this.setData({
-          "content" : content.content,
-          "time": content.content
+          OD_ADD: res.data.vision.OD_ADD,
+          OD_SPH: res.data.vision.OD_SPH,
+          OD_AX: res.data.vision.OD_AX,
+          OD_BCVA: res.data.vision.OD_BCVA,
+          OD_CYL: res.data.vision.OD_CYL,
+          OD_FR: res.data.vision.OD_FR,
+          OD_PR: res.data.vision.OD_PR,
+          OD_SPH: res.data.vision.OD_SPH,
+          OD_VA: res.data.vision.OD_VA,
+          OS_ADD: res.data.vision.OS_ADD,
+          OS_SPH: res.data.vision.OS_SPH,
+          OS_AX: res.data.vision.OS_AX,
+          OS_BCVA: res.data.vision.OS_BCVA,
+          OS_CYL: res.data.vision.OS_CYL,
+          OS_FR: res.data.vision.OS_FR,
+          OS_PR: res.data.vision.OS_PR,
+          OS_SPH: res.data.vision.OS_SPH,
+          OS_VA: res.data.vision.OS_VA,
+          PD: res.data.vision.PD,
+          store_function: res.data.vision.function,
+          remark: res.data.vision.remark,
+          store: res.data.store,
+          time: res.data.time
         })
-      }
+      },
     })
   },
-
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage() {
+
   }
 })
--- pages/after-salesList/after-salesList.js
+++ pages/after-salesList/after-salesList.js
@@ -1,10 +1,10 @@
+// pages/orderList/orderList.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
@@ -20,10 +20,9 @@ Page({
           method: 'GET',
           data: {
             'code': res.data.id,
-            'text': 'Service'
+            'text': 'Order'
           },
           success: (res) => {
-            console.log(res.data)
             that.setData({
               contentList: res.data.content
             })
@@ -41,56 +40,56 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage() {
+
   },
 
-  getReport(e) {
+  getOrder(e) {
     let code = e.target.dataset.bar_code
     wx.navigateTo({
-      url: '../after-sales/after-sales?code=' + code
+      url: '../order/order?code=' + code
     })
   },
-})
+});
